c8e5b5ccbecc290cecd08b0f00bf09b584dc3b11
Bruno Alves <nito.ba.dev@gmail.com>
2024-09-21 22:06:03 -0300

    chore: adding lint rules for imports

--- src/module/content/content.module.ts
+++ src/module/content/content.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common'
 
 import { HttpClient } from '@/contentModule/infra/http/http.client'
 
+import { ConfigModule } from '../../shared/module/config/config.module'
 import { ContentManagementService } from './core/service/content.management.service'
 import { MediaPlayerService } from './core/service/media-player.service'
 import { ExternalMovieClient } from './http/rest/client/external-movie-rating/external-movie-rating.client'
 import { MediaPlayerController } from './http/rest/controllers/media-player.controller'
 import { VideoUploadController } from './http/rest/controllers/video-upload.controller'
-import { ConfigModule } from '../../shared/module/config/config.module'
 import { PersistenceModule } from './persistence/persistence.module'
 import { ContentRepository } from './persistence/repository/content.repository'
 import { VideoRepository } from './persistence/repository/video.repository'
